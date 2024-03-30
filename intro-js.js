f7061cdacb84230100fcef014222f9f5a5ce0ee9
Ekthasurya <suryakiranmajhi1999@gmail.com>
2024-03-30 12:09:01 +0530

    Added Day 1 programs of Introduction to JS(variables) problems

---
+++ intro-js.js
@@ -0,0 +1,51 @@
+var a;
+a = 10;
+console.log(a);
+var a = 10;
+console.log(a);
+var name = "goku";
+console.log(name);
+console.log(typeof(name));
+
+var a = 4;
+var b = 7;
+console.log("The sum is ",a+b);
+console.log("The difference is ",a-b);
+console.log("The multiplication is ",a*b);
+console.log("The Division is ",a/b);
+
+//string concatenation
+
+var a = "Hello";
+var b = "World";
+
+var x = a+b;
+console.log(x);
+console.log(a +" "+ b);
+
+//Boolean Datatype
+
+var x = true;
+console.log(x);
+console.log(typeof(x));
+
+//Relational operator
+
+var goku = 6;
+var kurlin = 5;
+console.log(goku>kurlin);
+console.log(goku>=kurlin);
+console.log(goku<kurlin);
+console.log(goku<=kurlin);
+
+//comparison operator
+
+var a = "5";
+var b = 5;
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+console.log(a!==b);
+
+
+
